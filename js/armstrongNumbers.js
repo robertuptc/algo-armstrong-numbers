580a5dc46581b8a8c19410e778ae82929206d4a9
robertuptc <robertuptc@gmail.com>
2022-05-26 20:37:57 -0400

    exercised finished

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,16 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
-
+exports.findArmstrongNumbers = function(arr) {
+  let answer = []
+  
+  for (let i = 0; i < arr.length; i++) {
+    let newNum = arr[i].toString().split('').map((elem, _, arr) => elem ** arr.length).reduce((a,b) => a + b)
+    if (newNum === arr[i]) {
+      answer.push(newNum)
+    }  
+  } return answer
 };
+
+
+
+
